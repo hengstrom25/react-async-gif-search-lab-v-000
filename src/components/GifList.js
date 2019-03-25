4d073f2dc695d2ae8815d54241493b244e784444
Heidi Engstrom <hengstrom25@gmail.com>
2019-03-25 21:13:04 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,9 +1,15 @@
-import React from 'react'
- 
-const GifList = props => {
-  
-  return (
-    )
-}
- 
-export default GifList
+import React, { Component } from 'react';
+import shortid from 'shortid';
+
+ export default class GifList extends Component {
+    handleProps = () => {
+        return this.props.gifList.map(gif => { return <li key={shortid.generate()}><img src={gif} alt=""/></li> })
+    }
+    render() {
+        return (
+            <ul>
+                {this.handleProps()}
+            </ul>
+        )
+    }
+} 
